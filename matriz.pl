8e65a84beb5704837cf86a9f990a940e846635cc
Wykthor-Btracker <wykthor.g@wgvale.com>
2017-10-18 09:24:36 -0300

    Atualizando formato do buraco com ponte para pgp

--- matriz.pl
+++ matriz.pl
@@ -1,15 +1,23 @@
 :- dynamic matriz/1.
 matriz(
-        [p,c,p,c,p,c,p,p,
-         p,c,p,c,c,c,c,c,
-         p,c,p,c,p,c,p,p,
-         p,j,c,c,p,c,c,c,
-         p,p,p,c,p,p,c,p,
-         p,c,c,c,c,c,c,c,
-         p,c,p,p,c,p,c,p,
-         p,c,c,c,c,c,c,f]).
+        [p,c,p,c,p,c,p,p,p,c,p,c,p,c,p,p,
+         p,c,p,c,c,c,c,c,p,c,p,c,c,c,c,c,
+         p,c,p,c,p,c,p,p,p,c,p,c,p,c,p,p,
+         p,j,c,c,p,c,c,c,p,c,c,c,p,c,c,c,
+         p,c,p,c,p,p,c,p,p,p,p,c,p,p,c,p,
+         p,c,c,c,c,c,c,c,p,c,c,c,c,c,c,c,
+         p,c,p,p,c,p,c,p,p,c,p,p,c,p,c,p,
+         p,c,c,c,c,c,c,c,p,c,c,c,c,c,c,c,
+	 p,c,p,c,p,c,p,p,p,c,p,c,p,c,p,p,
+         p,c,p,c,c,c,c,c,p,c,c,c,c,c,c,c,
+         p,c,p,c,p,c,p,p,p,c,p,c,p,c,p,p,
+         p,c,c,c,p,c,c,c,p,c,c,c,p,c,c,c,
+         p,p,p,c,p,p,c,p,p,p,p,c,p,p,c,p,
+         p,c,c,c,c,c,c,c,p,c,c,c,c,c,c,c,
+         p,c,p,p,c,p,c,p,p,c,p,p,c,p,c,p,
+         p,c,c,c,c,c,c,c,p,c,c,c,c,c,c,c]).
 
-size(8).
+size(16).
 :- dynamic posicaoJogador/2,headingJogador/1.
 posicaoJogador(1,3).
 headingJogador(front).
