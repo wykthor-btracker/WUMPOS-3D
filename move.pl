83c47d86113b6fceb45fde1b409ae8873fd96b60
Wykthor-Btracker <wykthor.g@wgvale.com>
2017-10-25 18:16:15 -0300

    A saída não era acessível, agora é.

--- move.pl
+++ move.pl
@@ -6,6 +6,7 @@ replace([H|T], I, X, [H|R]):- I > -1, NI is I-1, replace(T, NI, X, R), !.
 replace(L, _, _, L).
 validMove(c).
 validMove(j).
+validMove(f).
 opposites(left,right).
 opposites(front,back).
 opposites(X,Y):- opposites(Y,X).
