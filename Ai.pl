e79b83634008f9984b9dbe7f5684af8ea18f648a
arqui456 <arquimedes9137@hotmail.com>
2017-10-24 23:53:11 -0200

    created skeleton AI file

---
+++ Ai.pl
@@ -0,0 +1,31 @@
+
+
+
+%%%%%%%%%%%% Artificial Intelligence %%%%%%%%%
+%                                            %
+%  The auto solving capabilities of the maze %
+%  will be implemented here, starting with   %
+%  some primordial functions:                %
+%                                            %
+%  depth first search(dfs)                   %
+%  &                                         %
+%  breadth first search(bfs)                 %
+%                                            %
+%                                            %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%% PROTOTYPES %%%%%%%%%%%%%%%%%%
+%                                             %
+% Place all the functions implemented in this %
+% file here, explain briefly what it does and %
+% its inputs.                                 %
+%                                             %
+%                                             %
+%                                             %
+%                                             %
+%                                             %
+%                                             %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
