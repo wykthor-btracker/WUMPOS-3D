9877c3adf76053d9144579fd841d0489eb95214a
Wykthor-Btracker <wykthor.g@wgvale.com>
2017-10-25 18:15:49 -0300

    10/10 depth-fist search

--- ia.pl
+++ ia.pl
@@ -1,4 +1,17 @@
 :- include('iaMove.pl').
-dfs(PosX,PosY,_,_):- final(PosX,PosY).
-dfs(PosX,PosY,Heading,R):- 
-	
+dfs(PosX,PosY,_,R):- saida(PosX,PosY),
+		     write(R).
+dfs(PosX,PosY,Heading,Current):- 
+	go(left,Heading,PosX,PosY,NewHeading,NewX,NewY),
+	append(Current,[left],Next),
+	dfs(NewX,NewY,NewHeading,Next).
+
+dfs(PosX,PosY,Heading,Current):- 
+	go(front,Heading,PosX,PosY,NewHeading,NewX,NewY),
+	append(Current,[front],Next),
+	dfs(NewX,NewY,NewHeading,Next).
+
+dfs(PosX,PosY,Heading,Current):- 
+	go(right,Heading,PosX,PosY,NewHeading,NewX,NewY),
+	append(Current,[right],Next),
+	dfs(NewX,NewY,NewHeading,Next).
