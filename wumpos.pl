e9c5fea50e07d4f8909c9f9f0abf73144bce5123
arqui456 <arquimedes9137@hotmail.com>
2017-10-24 23:27:54 -0200

    Converted all tabs into spaces

--- wumpos.pl
+++ wumpos.pl
@@ -4,10 +4,10 @@
 %:- include('inputUserHandler.pl').    <- still in beta
 
 wumpos :- 
-	    load(3),
-	    title,
-	    tutorial,
-	    ask.
+        load(3),
+        title,
+        tutorial,
+        ask.
 
 ask :- 
     write('Type command: '),
@@ -23,106 +23,106 @@ start('quit') :-
 start('start') :-
                write('which way do you wanna go ?'),
                nl,
-			   read(Y),
-			   process(Y).
+               read(Y),
+               process(Y).
 start(_) :-
-		 nl,
-		 write('Huh?'),
-		 nl,
-		 start('start').
+         nl,
+         write('Huh?'),
+         nl,
+         start('start').
 
 process('quit') :-
-				start('quit'),
-				!.
+                start('quit'),
+                !.
 process('look') :-
-			    current(jogador),
-			    start('start').
+                current(jogador),
+                start('start').
 process(Y) :-
-		   checkInput(Y),
-	       go(Y, jogador),
-    	   current(jogador),(
-		   \+check(jogador),
-    	   set(jogador),
-		   start('start')
-		   ;		   
-	       start('start')).
+           checkInput(Y),
+           go(Y, jogador),
+           current(jogador),(
+           \+check(jogador),
+           set(jogador),
+           start('start')
+           ;           
+           start('start')).
 
 checkInput('left').
 checkInput('right').
 checkInput('front').
 checkInput('back').
 checkInput(_) :-
- 		 write('Huh?... are you mentally challenged?'),
-		 nl,
-		 start('start').
+         write('Huh?... are you mentally challenged?'),
+         nl,
+         start('start').
 load(0).
 load(X):-
-	  X1 is X - 1,
+      X1 is X - 1,
       cls,
-	  fRead('./sprite/load1'),
-	  cls,
-	  sleep(0.2),
-	  fRead('./sprite/load2'),
-	  cls,
-	  sleep(0.2),
-	  fRead('./sprite/load3'),
-	  cls,
-	  sleep(0.2),
-	  cls,
-	  fRead('./sprite/load3'),
-	  cls,
-	  sleep(0.2),
-	  fRead('./sprite/load2'),
-	  cls,
-	  sleep(0.2),
-	  fRead('./sprite/load1'),
-	  cls,
-	  sleep(0.2),
-	  load(X1).
+      fRead('./sprite/load1'),
+      cls,
+      sleep(0.2),
+      fRead('./sprite/load2'),
+      cls,
+      sleep(0.2),
+      fRead('./sprite/load3'),
+      cls,
+      sleep(0.2),
+      cls,
+      fRead('./sprite/load3'),
+      cls,
+      sleep(0.2),
+      fRead('./sprite/load2'),
+      cls,
+      sleep(0.2),
+      fRead('./sprite/load1'),
+      cls,
+      sleep(0.2),
+      load(X1).
 title :-
-	   cls,
-	   fRead(title).
+       cls,
+       fRead(title).
 
 tutorial :-
-		  A = 'Wumpos is a very challenging game and require loads of dedication from the player, do you want to see the tutorial ?',
-		  B = 'Type yes or no: ',
-		  fancyWrite(red, A),
-		  nl,
-		  fancyWrite(green, B),
-		  read(Z),
-		  choice(Z).
+          A = 'Wumpos is a very challenging game and require loads of dedication from the player, do you want to see the tutorial ?',
+          B = 'Type yes or no: ',
+          fancyWrite(red, A),
+          nl,
+          fancyWrite(green, B),
+          read(Z),
+          choice(Z).
 
 choice('yes') :-
-			  A = 'All right chap, listen to me, this game is simple... why are you here? dont worry about that',
-			  B = ' RIGHT NOW. ',
-			  C = 'Listen, listen the commands are simple... just type the direction you wanna go to, you have four options:',
-			  D = ' left, right, front, back ',
-			  E = 'oh and i almost forgotten, beware, you are not',
-			  F = ' ALONE ',
-			  G = 'in this maze',
-			  H = ' as soon as you are ready type start, and the game shall begin',
-			  I = ' if you feel like giving up like the little bitch you are just type quit, and thats it for the tutorial', 
-			  J = ' (as soon as the misterious man finishes talking he disapears in the air) ',
-			  fancyWrite(red, A),
-			  fancyWrite(black, B),
-			  fancyWrite(red, C),
-			  fancyWrite(green, D),
-			  fancyWrite(red, E),
-			  fancyWrite(black, F),
-			  fancyWrite(red, G),
-			  fancyWrite(red, H),
-			  fancyWrite(red, I),
-			  fancyWrite(green,J),
-			  nl.
+              A = 'All right chap, listen to me, this game is simple... why are you here? dont worry about that',
+              B = ' RIGHT NOW. ',
+              C = 'Listen, listen the commands are simple... just type the direction you wanna go to, you have four options:',
+              D = ' left, right, front, back ',
+              E = 'oh and i almost forgotten, beware, you are not',
+              F = ' ALONE ',
+              G = 'in this maze',
+              H = ' as soon as you are ready type start, and the game shall begin',
+              I = ' if you feel like giving up like the little bitch you are just type quit, and thats it for the tutorial', 
+              J = ' (as soon as the misterious man finishes talking he disapears in the air) ',
+              fancyWrite(red, A),
+              fancyWrite(black, B),
+              fancyWrite(red, C),
+              fancyWrite(green, D),
+              fancyWrite(red, E),
+              fancyWrite(black, F),
+              fancyWrite(red, G),
+              fancyWrite(red, H),
+              fancyWrite(red, I),
+              fancyWrite(green,J),
+              nl.
 
 
 choice('no'). 
 
 choice('quit') :-
-				abort.
+                abort.
 choice(_) :-
-		   write('Huh ?, cant you even answer such a simple question ?'),
-		   nl,
-		   tutorial. 
+           write('Huh ?, cant you even answer such a simple question ?'),
+           nl,
+           tutorial. 
 
 %start :- get(name_asker('Register'), prompt, Name), write(Name), nl.  
