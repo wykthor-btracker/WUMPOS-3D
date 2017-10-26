cf36f3bbc2da89578aafcc52d5556b7e7b779fb6
Wykthor-Btracker <wykthor.g@wgvale.com>
2017-10-25 23:58:56 -0300

    Agora o dfs retorna a lista-resposta, em vez de imprimir

--- ia.pl
+++ ia.pl
@@ -1,17 +1,17 @@
 :- include('iaMove.pl').
-dfs(PosX,PosY,_,R):- saida(PosX,PosY),
-		     write(R).
-dfs(PosX,PosY,Heading,Current):- 
+dfs(PosX,PosY,_,R,R):- saida(PosX,PosY).
+
+dfs(PosX,PosY,Heading,Current,End):- 
 	go(left,Heading,PosX,PosY,NewHeading,NewX,NewY),
 	append(Current,[left],Next),
-	dfs(NewX,NewY,NewHeading,Next).
+	dfs(NewX,NewY,NewHeading,Next,End).
 
-dfs(PosX,PosY,Heading,Current):- 
+dfs(PosX,PosY,Heading,Current,End):- 
 	go(front,Heading,PosX,PosY,NewHeading,NewX,NewY),
 	append(Current,[front],Next),
-	dfs(NewX,NewY,NewHeading,Next).
+	dfs(NewX,NewY,NewHeading,Next,End).
 
-dfs(PosX,PosY,Heading,Current):- 
+dfs(PosX,PosY,Heading,Current,End):- 
 	go(right,Heading,PosX,PosY,NewHeading,NewX,NewY),
 	append(Current,[right],Next),
-	dfs(NewX,NewY,NewHeading,Next).
+	dfs(NewX,NewY,NewHeading,Next,End).
