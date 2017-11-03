edf4e7904ed607189c729b350e714cfc9725e385
Wykthor-Btracker <wykthor.g@wgvale.com>
2017-11-03 16:20:05 -0300

    Tirei os debugs do ia.pl

--- ia.pl
+++ ia.pl
@@ -4,23 +4,17 @@ dfs(PosX,PosY,_,_,R,R):- saida(PosX,PosY).
 dfs(PosX,PosY,Heading,Visited,Current,End):- 
 	go(left,Heading,PosX,PosY,NewHeading,NewX,NewY),
 	append(Current,[left],Next),
-	write(Next),nl,
-	sleep(0.5),
 	set(NewX,NewY,Visited,NewVisited),
 	dfs(NewX,NewY,NewHeading,NewVisited,Next,End).
 
 dfs(PosX,PosY,Heading,Visited,Current,End):- 
 	go(front,Heading,PosX,PosY,NewHeading,NewX,NewY),
 	append(Current,[front],Next),
-	write(Next),nl,
-	sleep(0.5),
 	set(NewX,NewY,Visited,NewVisited),
 	dfs(NewX,NewY,NewHeading,NewVisited,Next,End).
 
 dfs(PosX,PosY,Heading,Visited,Current,End):- 
 	go(right,Heading,PosX,PosY,NewHeading,NewX,NewY),!,
 	append(Current,[right],Next),
-	write(Next),nl,
-	sleep(0.5),
 	set(NewX,NewY,Visited,NewVisited),
 	dfs(NewX,NewY,NewHeading,NewVisited,Next,End).
