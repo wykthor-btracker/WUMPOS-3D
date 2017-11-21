a255ed1338795ab0d6346d508a6d57e892b599d7
arqui456 <arquimedes9137@hotmail.com>
2017-11-21 02:52:11 -0200

    added functions do the same thing of check and set but for planks

--- visited.pl
+++ visited.pl
@@ -1,12 +1,25 @@
-:- dynamic visited/1.
+:- dynamic visited/1,holeWithPlank/1.
 visited([]).
+holeWithPlank([]).
 
-check(Agent):- posicao(Agent,X,Y),
-	visited(List),
-	member([X,Y],List).
+check(Agent):- posicao(Agent,X,Y),  % later change this to checkMartini !!!
+    visited(List),
+    member([X,Y],List).
 
-set(Agent):- posicao(Agent,X,Y),
+set(Agent):- posicao(Agent,X,Y),  % later change this to setMartini !!!
       \+check(jogador),
       append([[X,Y]],List,R),
       retract(visited(List)),
       asserta(visited(R)).
+
+checkPlank(Agent):-
+                 posicao(Agent,X,Y),
+                 holeWithPlank(List),
+                 member([X,Y],List).
+
+setPlank(Agent):-
+                posicao(Agent,X,Y),
+                \+check(jogador),
+                append([[X,Y]],List,R),
+                retract(holeWithPlank(List)),
+                asserta(holeWithPlank(R)).
